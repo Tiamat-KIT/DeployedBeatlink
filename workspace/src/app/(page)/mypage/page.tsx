4462a06144dccc4ec06472791d2b0cba98b1f8a7
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-07-20 22:14:23 +0900

    マイページ作成

--- workspace/src/app/(page)/mypage/page.tsx
+++ workspace/src/app/(page)/mypage/page.tsx
@@ -1,31 +1,53 @@
-import React from 'react'
-import NameId from '../../../compoents/name-id'
-import PostsView from '../../../compoents/posts-view'
+import NameId from "../../../compoents/name-id";
+import PostsView from "../../../compoents/posts-view";
+import Genre from "../../../compoents/genre";
+import Recent from "../../../compoents/recent";
 
 const page = () => {
   return (
-    <div style={{
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        flexDirection:"column"
-        }}>
-        <div style={{display:"flex",alignItems:"center"}}>
-            <img  src='./arrow-black-image.png'/>
-                <p  className=" w-screen flex justify-center items-center"
-                style={{
-                    fontWeight:"bold", 
-                    fontSize:"30px",
-                    marginTop:"20px"
-                    }}>
-                    マイページ
-                </p>
+    <>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src="./arrow-black-image.png" />
+          <p
+            className=" w-screen flex justify-center items-center"
+            style={{
+              fontWeight: "bold",
+              fontSize: "30px",
+              marginTop: "20px",
+            }}
+          >
+            マイページ
+          </p>
         </div>
-        <NameId name="名前" id="id"/>
+        <NameId name="名前" id="id" />
         <PostsView />
-        
-    </div>
-  )
-}
+        <div className="mt-10">
+          <Genre />
+        </div>
+        <Recent />
+        <button
+          className="btn"
+          style={{
+            fontWeight: "lighter",
+            margin: "100px",
+          }}
+        >
+          アカウントを編集する
+        </button>
+        <a style={{ color: "red", marginBottom: "100px" }} href="./login">
+          ログアウト
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
