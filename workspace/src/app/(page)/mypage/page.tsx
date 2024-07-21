131450280de3705f6c60014ce8d8720262124e95
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-07-21 14:45:30 +0900

    NavBerつけた

--- workspace/src/app/(page)/mypage/page.tsx
+++ workspace/src/app/(page)/mypage/page.tsx
@@ -1,50 +1,59 @@
 /* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable @next/next/no-img-element */
-import NameId from '../../../components/name-id'
-import PostsView from '../../../components/posts-view'
-import Genre from '../../../components/genre'
-import Recent from '../../../components/recent'
+import NameId from "../../../components/name-id";
+import PostsView from "../../../components/posts-view";
+import Genre from "../../../components/genre";
+import Recent from "../../../components/recent";
+import NavBar from "@/components/NavBar";
 
 const page = () => {
   return (
     <>
-    <div style={{
-        display:"flex",
-        alignItems:"center",
-        justifyContent:"center",
-        flexDirection:"column"
-        }}>
-        <div style={{display:"flex",alignItems:"center"}}>
-            <img  src='./arrow-black-image.png'/>
-                <p  className=" w-screen flex justify-center items-center"
-                style={{
-                    fontWeight:"bold", 
-                    fontSize:"30px",
-                    marginTop:"20px"
-                    }}>
-                    マイページ
-                </p>
+      <NavBar />
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div style={{ display: "flex", alignItems: "center" }}>
+          <img src="./arrow-black-image.png" />
+          <p
+            className=" w-screen flex justify-center items-center"
+            style={{
+              fontWeight: "bold",
+              fontSize: "30px",
+              marginTop: "20px",
+            }}
+          >
+            マイページ
+          </p>
         </div>
-        <NameId name="名前" id="id"/>
+        <NameId name="名前" id="id" />
         <PostsView />
-        <div className='mt-10'>
-         <Genre />
+        <div className="mt-10">
+          <Genre />
         </div>
         <Recent />
-        <a href='./edit-mypage'>
-        <button className="btn"
-        style={{
-          fontWeight:"lighter",
-           margin:"100px"
-          }}
-        >アカウントを編集する</button>
+        <a href="./edit-mypage">
+          <button
+            className="btn"
+            style={{
+              fontWeight: "lighter",
+              margin: "100px",
+            }}
+          >
+            アカウントを編集する
+          </button>
         </a>
-        <a style={{color:"red",marginBottom:"100px"}} href='./login'>ログアウト</a>
-        </div>
-  </>
-        
-    
-  )
-}
+        <a style={{ color: "red", marginBottom: "100px" }} href="./login">
+          ログアウト
+        </a>
+      </div>
+    </>
+  );
+};
 
-export default page
+export default page;
