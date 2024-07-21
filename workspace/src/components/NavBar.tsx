cd224b128a92837fd58db6818ab4c35c01ea5a7b
ogaoga08 <kamekameshin0514@gmail.com>
2024-07-21 12:01:57 +0900

    ログアウト・プロフィールボタンの設置

--- workspace/src/components/NavBar.tsx
+++ workspace/src/components/NavBar.tsx
@@ -1,3 +1,4 @@
+import { UserButton } from "@clerk/nextjs"
 import type React from "react"
 import { useState } from "react"
 
@@ -21,39 +22,43 @@ const NavBar: React.FC = () => {
 						alt="Flowbite Logo"
 					/>
 					<span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-						Beetlink
+						Beatlink
 					</span>
 				</a>
-				<button
-					onClick={HandleToggle}
-					type="button"
-					className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-					aria-controls="navbar-default"
-					aria-expanded={IsOpen ? "true" : "false"}
-				>
-					<span className="sr-only">Open main menu</span>
-					<svg
-						className="w-5 h-5"
-						aria-hidden="true"
-						xmlns="http://www.w3.org/2000/svg"
-						fill="none"
-						viewBox="0 0 17 14"
+				<div className="flex items-center md:hidden space-x-2">
+					<button
+						onClick={HandleToggle}
+						type="button"
+						className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+						aria-controls="navbar-default"
+						aria-expanded={IsOpen ? "true" : "false"}
 					>
-						<path
-							stroke="currentColor"
-							strokeLinecap="round"
-							strokeLinejoin="round"
-							strokeWidth="2"
-							d="M1 1h15M1 7h15M1 13h15"
-						/>
-					</svg>
-				</button>
+						<span className="sr-only">Open main menu</span>
+						<svg
+							className="w-5 h-5"
+							aria-hidden="true"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							viewBox="0 0 17 14"
+						>
+							<path
+								stroke="currentColor"
+								strokeLinecap="round"
+								strokeLinejoin="round"
+								strokeWidth="2"
+								d="M1 1h15M1 7h15M1 13h15"
+							/>
+						</svg>
+					</button>
+
+					<UserButton afterSignOutUrl="/" />
+				</div>
 				<div
 					className={`w-full md:block md:w-auto ${IsOpen ? "block" : "hidden"}`}
 					id="navbar-default"
 				>
 					<ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-						<li>
+						<li className="flex items-center">
 							<a
 								href="/list"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -62,7 +67,7 @@ const NavBar: React.FC = () => {
 								Posts
 							</a>
 						</li>
-						<li>
+						<li className="flex items-center">
 							<a
 								href="/create"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -70,7 +75,7 @@ const NavBar: React.FC = () => {
 								Create post
 							</a>
 						</li>
-						<li>
+						<li className="flex items-center">
 							<a
 								href="/play"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
@@ -78,13 +83,16 @@ const NavBar: React.FC = () => {
 								Music
 							</a>
 						</li>
-						<li>
+						<li className="flex items-center">
 							<a
 								href="/mypage"
 								className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
 							>
 								My page
 							</a>
+						</li>
+						<li className="hidden md:block">
+							<UserButton afterSignOutUrl="/" />
 						</li>
 					</ul>
 				</div>
