c7cded2ab49fb7955f4dd45e7749e86926f2d712
chiikawasaki <7saki-56chu@ezweb.ne.jp>
2024-07-21 13:46:45 +0900

    マイページ、マイページ編集画面作成

---
+++ workspace/src/app/(page)/edit-mypage/page.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import EditNameid from "../../../components/edit-nameid";
+import CustomButton from "@/components/custom-button";
+import { SignOutButton } from "@clerk/nextjs";
+
+const page = () => {
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <EditNameid />
+        <div className="mt-10"></div>
+        <CustomButton text="変更を保存" href="./mypage" />
+      </div>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          margin: "30px",
+        }}
+      >
+        <SignOutButton>
+          <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+            ログアウト
+          </button>
+        </SignOutButton>
+      </div>
+    </div>
+  );
+};
+
+export default page;
